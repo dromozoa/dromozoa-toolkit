604ba4244cc7e4694eeba0d8dd581db2df687be6
Tomoyuki Fujimori <moyu@dromozoa.com>
2023-04-24 06:23:11 +0900

    Matrix3を追加。

--- dtk.js
+++ dtk.js
@@ -38,6 +38,80 @@ const numberToCss = (v, unit = "px") => {
 
 //-------------------------------------------------------------------------
 
+const Matrix3 = class {
+  constructor(...params) {
+    this.set(...params);
+  }
+
+  clone() {
+    return new Matrix3(this);
+  }
+
+  set(A, m12, m13, m21, m22, m23, m31, m32, m33) {
+    if (m12 === undefined) {
+      const { m11, m12, m13, m21, m22, m23, m31, m32, m33 } = A;
+      this.m11 = m11; this.m12 = m12; this.m13 = m13;
+      this.m21 = m21; this.m22 = m22; this.m23 = m23;
+      this.m31 = m31; this.m32 = m32; this.m33 = m33;
+      return this;
+    } else {
+      const m11 = A;
+      this.m11 = m11; this.m12 = m12; this.m13 = m13;
+      this.m21 = m21; this.m22 = m22; this.m23 = m23;
+      this.m31 = m31; this.m32 = m32; this.m33 = m33;
+      return this;
+    }
+  }
+
+  setIdentity() {
+    return this.set(1, 0, 0, 0, 1, 0, 0, 0, 1);
+  }
+
+  invert(A = this) {
+    const { m11, m12, m13, m21, m22, m23, m31, m32, m33 } = A;
+    const a = m22 * m33 - m23 * m32;
+    const b = m23 * m31 - m21 * m33;
+    const c = m21 * m32 - m22 * m31;
+    const d = m11 * a + m12 * b + m13 * c;
+    if (d !== 0) {
+      const s = 1 / d;
+      return this.set(
+        a * s, (m13 * m32 - m12 * m33) * s, (m12 * m23 - m13 * m22) * s,
+        b * s, (m11 * m33 - m13 * m31) * s, (m13 * m21 - m11 * m23) * s,
+        c * s, (m12 * m31 - m11 * m32) * s, (m11 * m22 - m12 * m21) * s);
+    }
+  }
+
+  mul(A, B) {
+    if (B === undefined) {
+      B = A;
+      A = this;
+    }
+    const {
+      m11: a11, m12: a12, m13: a13,
+      m21: a21, m22: a22, m23: a23,
+      m31: a31, m32: a32, m33: a33,
+    } = A;
+    const {
+      m11: b11, m12: b12, m13: b13,
+      m21: b21, m22: b22, m23: b23,
+      m31: b31, m32: b32, m33: b33,
+    } = B;
+    return this.set(
+      a11 * b11 + a12 * b21 + a13 * b31,
+      a11 * b12 + a12 * b22 + a13 * b32,
+      a11 * b13 + a12 * b23 + a13 * b33,
+      a21 * b11 + a22 * b21 + a23 * b31,
+      a21 * b12 + a22 * b22 + a23 * b32,
+      a21 * b13 + a22 * b23 + a23 * b33,
+      a31 * b11 + a32 * b21 + a33 * b31,
+      a31 * b12 + a32 * b22 + a33 * b32,
+      a31 * b13 + a32 * b23 + a33 * b33);
+  }
+};
+
+//-------------------------------------------------------------------------
+
 const FrameRate = class {
   constructor(size) {
     this.size = size;
@@ -105,7 +179,7 @@ const guiObject = {
   fps: 0,
   fpsMin: 0,
   fpsMax: 0,
-  tool: "move",
+  tool: "select",
 };
 
 let gui;
@@ -145,6 +219,7 @@ const updateGui = () => {
 
 const changeTool = tool => {
   document.querySelector(".dtk-canvas").style.cursor = toolCursors[tool];
+  canvasObject.mouseDown = undefined;
 };
 
 const updateTool = tool => {
@@ -199,6 +274,8 @@ const initialize = () => {
   gui.add(guiObject, "fpsMin").name("最小FPS");
   gui.add(guiObject, "fpsMax").name("最大FPS");
   gui.add(guiObject, "tool", toolLabels).name("ツール").onChange(changeTool);
+
+  changeTool(guiObject.tool);
 };
 
 const onKeyDown = ev => {
